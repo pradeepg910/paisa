37f1a8ff4c14c05f8b58442d5d6a17f925ed11f7
pradeepg910 <pradi910@gmail.com>
2017-04-22 09:40:21 -0400

    validation for register form and fixed alignment issues

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ToastController, NavController } from 'ionic-angular';
 import {AngularFire} from 'angularfire2';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { LoginPage } from '../login/login';
 import { TabsPage } from '../tabs/tabs';
 
@@ -16,10 +16,16 @@ export class RegistrationPage {
               public toastCtrl:ToastController,
               private formBuilder: FormBuilder) {
     this.registerForm = formBuilder.group({
-      email: ['', Validators.compose([Validators.required])],
-      name: ['', Validators.compose([Validators.required])],
-      username: ['', Validators.compose([Validators.required])]
+      email: ['', Validators.compose([Validators.maxLength(50), Validators.required, this.isEmail])],
+      name: ['', Validators.compose([Validators.maxLength(50), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      username: ['', Validators.compose([Validators.maxLength(20), Validators.minLength(6), Validators.required])]
     });
+  }
+
+  isEmail(control: FormControl): {[s: string]: boolean} {
+    if (!control.value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+      return {noEmail: true};
+    }
   }
 
   cancel() {
